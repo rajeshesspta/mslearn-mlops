e5ea39d2133d96cddcba6d757e2ba3b9fbba75f7
Rajesh S <rajeshesspta@gmail.com>
2024-10-13 17:41:17 +0530

    added mlflow logging, split function

--- src/model/train.py
+++ src/model/train.py
@@ -5,14 +5,14 @@ import glob
 import os
 
 import pandas as pd
-
+import mlflow
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,7 +34,15 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    X = df.drop('Y', axis=1).values
+    y = df['Y'].values
 
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=0)
+    data = {"train": {"X": X_train, "y": y_train},
+            "test": {"X": X_test, "y": y_test}}
+    return data
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
